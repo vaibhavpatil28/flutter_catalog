b0f5f1f14546d0b7dc850d860adb89a6445aab71
Vaibhav <vaibhavpatil2891@gmail.com>
2022-06-26 20:07:53 +0530

    day12: home page UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widget/item_widget.dart';
 
 import '../widget/drawer.dart';
 
@@ -6,12 +8,17 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, ((index) => CatalogModel.items[0]));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog'),
       ),
-      body: const Center(
-        child: Text("welcome to flutter"),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index){
+          return ItemWidget(item: dummyList[index]);
+        },
       ),
       drawer: MyDrawer(),
     );
---
+++ lib/widget/item_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price}",
+          textScaleFactor: 1.5,
+          style: TextStyle(
+            color: Colors.deepOrange.shade500,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+    );
+  }
+}
